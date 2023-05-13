c8686c2de5de4a725323bd28de3e23d1ca7f8965
gaurav-uk2001 <gaurav.vaishali2001@gmail.com>
2023-05-13 09:07:39 +0530

    Use effect start

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,56 +1,24 @@
 // ----------------------REACT HOOKS
 
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 
 const Home = () =>{
 
     const [a,setA] = useState(0);
 
-    // let a=0;
-
-    // const increment = () =>{
-    //     setA(a+1);
-    //     // a = a+1;
-    //     console.log(a);
-    // }
-
-    const decrement = () =>{
-        setA(a-1);
-        // a = a-1;
-        console.log(a);
-    }
+    // call back function
+    // run when it is mount and when it is update
+    useEffect(()=>{
+        console.log("showing effect");
+    },[a])
+    // dependency array [a]: mean it run when a or other variable b,c,d(if mentioned) call
 
     return(
         <div>
             <button onClick={()=>setA(a+1)}>Add</button>
             <p>{a}</p>
-            <button onClick={decrement}>Minus</button>
+            <button onClick={()=>setA(a-1)}>Minus</button>
         </div>
     )
 }
 export default Home
-
-
-
-// import React , {useEffect, useState} from "react";
-
-// const Home = () =>{
-
-//     const [a,setA] = useState(0);
-
-//     // call back function
-//     // run when it is mount and when it is update
-//     useEffect(()=>{
-//         console.log("showing effect");
-//     },[a])
-//     // dependency array [a]: mean it run when a call
-
-//     return(
-//         <div>
-//             <button onClick={()=>setA(a+1)}>Add</button>
-//             <p>{a}</p>
-//             <button onClick={()=>setA(a-1)}>Minus</button>
-//         </div>
-//     )
-// }
-// export default Home
